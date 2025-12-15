a0e3938fb1e1c653c80cd2cb4662aed66d66545e
BLAKEJEWELL <bjewell1833@gmail.com>
2025-12-15 14:55:32 -0500

    Added a date validation function. last day overload function done.

--- Date.cpp
+++ Date.cpp
@@ -37,9 +37,38 @@ int lastDay()
 {
 
 }
-int lastDay(int month, int year)
+int lastDay(int m, int y)
 {
+	int t = m;
+	
+	switch (m)
+	{
+	case 1: 
+		return 31;
+	case 3:
+		return 31;
+	case 5:
+		return 31;
+	case 7:
+		return 31;
+	case 8:
+		return 31;
+	case 10:
+		return 31;
+	case 12:
+		return 31;
+	case 4:
+		return 30;
+	case 6:
+		return 30;
+	case 9:
+		return 30;
+	case 11:
+		return 30;
+	case 2:
+		return isleapyear(y);
 
+	}
 }
 
 string datedisplay1() // format: month day year
@@ -53,4 +82,17 @@ string datedisplay2() // format: day month year
 string datedisplay3() // format: 00/00/0000
 {
 
+}
+bool datevalidation(int m, int d, int y)
+{
+	if (m < 1 || m>12)
+	{
+		return false;
+	}
+	if (y < 1)
+	{
+		return false;
+	}
+
+	int howmanydays = lastDay(m, y);
 }
